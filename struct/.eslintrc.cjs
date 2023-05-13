1a58d80c10ec042865c493d3f4b11f464bbc66f3
BetaLixT <39497438+BetaLixT@users.noreply.github.com>
2023-05-13 08:57:54 +0400

    wip

--- struct/.eslintrc.cjs
+++ struct/.eslintrc.cjs
@@ -13,13 +13,24 @@ const config = {
         project: path.join(__dirname, "tsconfig.json"),
       },
     },
+    {
+      files: ["*.stories.@(ts|tsx|js|jsx|mjs|cjs)"],
+      rules: {
+        // example of overriding a rule
+        "storybook/hierarchy-separator": "error",
+      },
+    },
   ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     project: path.join(__dirname, "tsconfig.json"),
   },
   plugins: ["@typescript-eslint"],
-  extends: ["next/core-web-vitals", "plugin:@typescript-eslint/recommended"],
+  extends: [
+    "next/core-web-vitals",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:storybook/recommended",
+  ],
   rules: {
     "@typescript-eslint/consistent-type-imports": [
       "warn",
@@ -28,8 +39,12 @@ const config = {
         fixStyle: "inline-type-imports",
       },
     ],
-    "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+    "@typescript-eslint/no-unused-vars": [
+      "warn",
+      {
+        argsIgnorePattern: "^_",
+      },
+    ],
   },
 };
-
 module.exports = config;
---
+++ struct/src/ui/templates/base/BaseTemplate.tsx
@@ -0,0 +1,7 @@
+import { FC } from "react";
+
+export interface IBaseTemplate { }
+
+const BaseTemplate: FC<IBaseTemplate> = () => <div>Hello World</div>;
+
+export default BaseTemplate;
