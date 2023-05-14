700d1ed2a4ab15e2742c50dc69ab334258c6ee2b
BetaLixT <39497438+BetaLixT@users.noreply.github.com>
2023-05-14 23:12:15 +0400

    wip, cleanups

---
+++ turborepo/packages/domains/core/index.tsx
@@ -0,0 +1,3 @@
+import * as React from "react";
+import "ui/styles.css";
+export * from "./introduction/index";
---
+++ turborepo/packages/domains/core/introduction/index.tsx
@@ -0,0 +1,26 @@
+import { FC } from "react";
+
+type Props = {};
+
+export const Introduction: FC<Props> = () => {
+  return (
+    <>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <Button
+          onClick={() => setCount((count) => count + 1)}
+          buttonType={ButtonType.Warn}
+        />
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+};
