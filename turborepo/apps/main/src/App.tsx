55689c17b4091d62c3bb444d00146e3b2e196b35
BetaLixT <39497438+BetaLixT@users.noreply.github.com>
2023-05-14 13:29:48 +0400

    tsconfig update

--- turborepo/apps/main/src/App.tsx
+++ turborepo/apps/main/src/App.tsx
@@ -1,10 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { Button, ButtonType } from "ui";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -21,6 +22,10 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
+        <Button
+          onClick={() => setCount((count) => count + 1)}
+          buttonType={ButtonType.Warn}
+        />
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
@@ -29,7 +34,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
