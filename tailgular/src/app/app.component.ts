a69ad3a81edb7c039952bd5fb8877a37cc67b70d
BetaLixT <39497438+BetaLixT@users.noreply.github.com>
2024-01-13 14:44:28 +0400

    implementation of row calc

--- tailgular/src/app/app.component.ts
+++ tailgular/src/app/app.component.ts
@@ -1,22 +1,61 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { MessagesComponent } from './messages/messages.component';
 import { MessageService } from './message.service';
+import { RowComponent } from './row-calc/row/row.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, MessagesComponent],
+  imports: [CommonModule, RouterOutlet, MessagesComponent, RowComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'tailgular';
+  @ViewChild(RowComponent) rowComponent!: RowComponent;
+  rows: RowData[] = [];
+  sum = 0;
 
   constructor(private messageService: MessageService) { }
 
+  addRow() {
+    let row = new RowData();
+    row.valueUpdated.subscribe(() => {
+      this.calculateSum();
+    });
+    this.rows.push(row);
+  }
+
+  calculateSum() {
+    let sum = 0;
+    for (let row of this.rows) {
+      sum += row.value;
+    }
+
+    this.messageService.add(`new sum ${sum}`);
+    this.sum = sum;
+  }
+
   ngOnInit(): void {
     this.messageService.add('app ng on init');
+  }
+
+  valueUpdate(val: number) {
+    this.messageService.add(`updated value ${val}`);
+  }
+
+  ngAfterViewInit() {
+    // this.rowComponent.inputNumber = 3;
+  }
+}
+
+class RowData {
+  value = 0;
+  valueUpdated = new EventEmitter<number>();
+  updateValue(val: number) {
+    this.value = val;
+    this.valueUpdated.next(this.value);
   }
 }
---
+++ tailgular/src/app/row-calc/row/row.component.ts
@@ -0,0 +1,38 @@
+import { Component, EventEmitter, Output, SimpleChanges } from '@angular/core';
+import { NgFor } from '@angular/common';
+import { MessageService } from '../../message.service';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'row-calc-row',
+  standalone: true,
+  imports: [NgFor, ReactiveFormsModule],
+  templateUrl: './row.component.html',
+})
+export class RowComponent {
+  @Output() rowValue = new EventEmitter<number>();
+
+  inputNumber = new FormControl<undefined | number>(undefined);
+  inputOper = new FormControl<string>('add');
+  constructor(private messageService: MessageService) {
+    this.inputNumber.valueChanges.subscribe(() => {
+      this.emitValue();
+    });
+
+    this.inputOper.valueChanges.subscribe(() => {
+      this.emitValue();
+    });
+  }
+
+  emitValue() {
+    const ival = this.inputNumber.getRawValue();
+    const iopr = this.inputOper.getRawValue();
+    let val = 0;
+    if (ival) {
+      val = iopr == 'add' ? ival : ival * -1;
+    }
+
+    this.rowValue.next(val);
+    return;
+  }
+}
